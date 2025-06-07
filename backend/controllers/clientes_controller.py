5894ffc5fa3efcf22155bcc04d2c145186fb6b6c
joaco2026 <joaquinhernandezdusio@gmail.com>
2025-06-07 19:12:20 -0300

    Update clientes_controller.py

--- backend/controllers/clientes_controller.py
+++ backend/controllers/clientes_controller.py
@@ -2,7 +2,7 @@
 
 import mysql.connector
 from backend.db.conexion import crear_conexion, cerrar_conexion
-from backend.db.Queries.clientes-queries import (
+from backend.db.queries.clientes-queries import (
     INSERTAR_CLIENTE,
     EDITAR_CLIENTE,
     ELIMINAR_CLIENTE,
