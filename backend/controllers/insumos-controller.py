51beae2c963266fbc284dbbd0aff2a5579e0b8bb
joaco2026 <joaquinhernandezdusio@gmail.com>
2025-06-07 19:13:32 -0300

    Update insumos-controller.py

--- backend/controllers/insumos-controller.py
+++ backend/controllers/insumos-controller.py
@@ -1,1 +1,10 @@
-#hola
+#Maneja altas, bajas, modificaciones y consultas de insumos.(insert, update, delete y select)
+
+import mysql.connector
+from backend.db.conexion import crear_conexion, cerrar_conexion
+from backend.db.queries.insumos-queries import (
+    INSERTAR_INSUMO,
+    EDITAR_INSUMO,
+    ELIMINAR_INSUMO,
+    LISTAR_INSUMO
+)
