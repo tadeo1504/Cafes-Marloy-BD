540d0edcd8adf33ba54614493e11387bfc2d8780
joaco2026 <joaquinhernandezdusio@gmail.com>
2025-06-07 19:05:48 -0300

    Update mantenimientos_controller.py

--- backend/controllers/mantenimientos_controller.py
+++ backend/controllers/mantenimientos_controller.py
@@ -1,15 +1,14 @@
 #administrar alta baja modificacion y consulta de mantenimientos
-##es decir insert, update, delete y select
+#es decir insert, update, delete y select
 
 import mysql.connector
 from backend.db.conexion import crear_conexion, cerrar_conexion
-#primero tiene que existir el archivo mantenimientos_querys.py en la carpeta db
-# from backend.db.mantenimientos_querys import (
-#     INSERTAR_MANTENIMIENTO,
-#     EDITAR_MANTENIMIENTO,
-#     ELIMINAR_MANTENIMIENTO,
-#     LISTAR_MANTENIMIENTOS
-# )
+from backend.db.queries.mantenimientos-queries import (
+    INSERTAR_MANTENIMIENTO,
+    EDITAR_MANTENIMIENTO,
+    ELIMINAR_MANTENIMIENTO,
+    LISTAR_MANTENIMIENTOS
+ )
 
 def alta_mantenimiento():
     pass
