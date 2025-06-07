72a45428d2ce7b60581f7a69bc7ba5bbb12bbe76
joaco2026 <joaquinhernandezdusio@gmail.com>
2025-06-07 20:58:39 -0300

    Update insumos-controller.py

--- backend/controllers/insumos-controller.py
+++ backend/controllers/insumos-controller.py
@@ -8,3 +8,19 @@ from backend.db.queries.insumos-queries import (
     ELIMINAR_INSUMO,
     LISTAR_INSUMO
 )
+
+def alta_insumo():
+    print("=== Alta de Insumo ===")
+    descripcion = input("Descripción: ")
+    tipo = input("Tipo: ")
+    precio_unitario = input("Precio unitario: ")
+    id_proveedor = input("Id del Proveedor: ")
+
+    conexion = crear_conexion()
+    if conexion:
+        exito = insertar_insumo(conexion, nombre, direccion, telefono, correo)
+        cerrar_conexion(conexion)
+        if exito:
+            print("Insumo insertado correctamente.")
+        else:
+            print("No se pudo insertar el insumo.")
