e6c2be2faf2dd74c2bc30fa5753c67b925003bb1
joaco2026 <joaquinhernandezdusio@gmail.com>
2025-06-14 23:18:24 -0300

    Update insumos_controller.py

--- backend/controllers/insumos_controller.py
+++ backend/controllers/insumos_controller.py
@@ -26,3 +26,45 @@ def alta_insumo():
             print("No se pudo insertar el insumo.")
     
     
+def modificar_insumo():
+    print("=== Modificación de Insumo ===")
+    id = input("ID del Insumo a modificar: ")
+    nombre = input("Nuevo Nombre: ")
+    direccion = input("Nueva Dirección: ")
+    telefono = input("Nuevo Teléfono: ")
+    correo = input("Nuevo Correo: ")
+
+    conexion = crear_conexion()
+    if conexion:
+        exito = editar_insumo(conexion, nombre, direccion, telefono, correo, id)
+        cerrar_conexion(conexion)
+        if exito:
+            print("✅ Insumo modificado correctamente.")
+        else:
+            print("❌ No se pudo modificar el insumo.")
+
+def baja_insumo():
+    print("=== Baja de Insumo ===")
+    id = input("ID del Insumo a eliminar: ")
+
+    conexion = crear_conexion()
+    if conexion:
+        exito = eliminar_insumo(conexion, id)
+        cerrar_conexion(conexion)
+        if exito:
+            print("✅ Insumo eliminado correctamente.")
+        else:
+            print("❌ No se pudo eliminar el insumo.")
+
+def listar_insumos():
+    print("=== Lista de Insumos ===")
+    conexion = crear_conexion()
+    if conexion:
+        clientes = mostrar_insumos(conexion)
+        cerrar_conexion(conexion)
+
+        if insumos:
+            for i in insumos:
+                print(f"ID: {i[0]} | Nombre: {i[1]} | Dirección: {c[2]} | Tel: {c[3]} | Correo: {c[4]}")
+        else:
+            print("No se encontraron insumos.")
