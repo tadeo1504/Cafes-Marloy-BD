4999eea8dc9c5e9dfae511cd2ab2043119453fcb
joaco2026 <joaquinhernandezdusio@gmail.com>
2025-06-15 13:14:09 -0300

    Update proveedores_controller.py

--- backend/controllers/proveedores_controller.py
+++ backend/controllers/proveedores_controller.py
@@ -6,13 +6,50 @@ from backend.db.conexion import crear_conexion, cerrar_conexion
 
 
 def insertar_proveedor():
-    pass
+    print("=== Insertar Proveedor ===")
+    nombre = input("Nombre: ")
+    direccion = input("Dirección: ")
+    telefono = input("Teléfono: ")
+    correo = input("Correo: ")
 
-def editar_proveedor():
-    pass
+    conexion = crear_conexion()
+    if conexion:
+        exito = insertar_proveedor(conexion, nombre, direccion, telefono, correo)
+        cerrar_conexion(conexion)
+        if exito:
+            print("✅ Proveedor insertado correctamente.")
+        else:
+            print("❌ No se pudo insertar el proveedor.")
 
-def eliminar_proveedor():
-    pass
+def modificar_proveedor():
+    print("=== Modificación de Proveedor ===")
+    id = input("ID del Proveedor a modificar: ")
+    nombre = input("Nuevo Nombre: ")
+    direccion = input("Nueva Dirección: ")
+    telefono = input("Nuevo Teléfono: ")
+    correo = input("Nuevo Correo: ")
+
+    conexion = crear_conexion()
+    if conexion:
+        exito = editar_cliente(conexion, nombre, direccion, telefono, correo, id)
+        cerrar_conexion(conexion)
+        if exito:
+            print("✅ Cliente modificado correctamente.")
+        else:
+            print("❌ No se pudo modificar el cliente.")
+
+def baja_proveedor():
+    print("=== Baja de Proveedor ===")
+    id = input("ID del Proveedor a eliminar: ")
+
+    conexion = crear_conexion()
+    if conexion:
+        exito = eliminar_cliente(conexion, id)
+        cerrar_conexion(conexion)
+        if exito:
+            print("✅ Proveedor eliminado correctamente.")
+        else:
+            print("❌ No se pudo eliminar el proveedor.")
 
 def listar_proveedores():
     # Crear conexión
